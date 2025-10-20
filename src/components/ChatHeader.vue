359817b6694608c6a26e9e1ad641aedffcf2bc7f
SherryLin050 <tingpark@gmail.com>
2025-10-20 14:22:29 +0800

    修改

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -27,56 +27,6 @@
                 </CDropdownMenu>
             </CDropdown>
         </div>
-        <!-- <template v-if="!isMoblie">
-            <div class="chatHeaderLogo">
-                <img
-                    src="/images/096bca4d-b3d4-4087-9e74-6d534396cf97.png"
-                    alt="Logo"
-                    class="chatHeaderLogoImg"
-                />
-            </div>
-            <div class="chatHeaderContent">
-                <h2 class="chatHeaderTitle">{{ title }}</h2>
-            </div>
-            <div class="d-flex justify-content-end">
-                <div class="chatStatus d-flex align-items-center small">
-                    <span :class="getStatusIndicatorClass()"></span>
-                    {{ connectionStatusText }}
-                </div>
-                <CDropdown variant="nav-item" dark>
-                    <CDropdownToggle :caret="false" class="changeMenu"><i class="bi bi-list"></i></CDropdownToggle>
-                    <CDropdownMenu>
-                        <CDropdownItem href="#">設定檔1</CDropdownItem>
-                        <CDropdownItem href="#">設定檔2</CDropdownItem>
-                    </CDropdownMenu>
-                </CDropdown>
-            </div>
-        </template>
-        <template v-else>
-            <div class="w-100 chatHeaderLogo">
-                <img
-                    src="/images/096bca4d-b3d4-4087-9e74-6d534396cf97.png"
-                    alt="Logo"
-                    class="chatHeaderLogoImg"
-                />
-            </div>
-            <div class="w-100 d-flex justify-content-between align-items-center">
-                <h2 class="chatHeaderTitle">{{ title }}</h2>
-                <div class="d-flex justify-content-end">
-                    <div class="chatStatus d-flex align-items-center small">
-                        <span :class="getStatusIndicatorClass()"></span>
-                        {{ connectionStatusText }}
-                    </div>
-                    <CDropdown variant="nav-item" dark>
-                        <CDropdownToggle :caret="false" class="changeMenu"><i class="bi bi-list"></i></CDropdownToggle>
-                        <CDropdownMenu>
-                            <CDropdownItem href="#">設定檔1</CDropdownItem>
-                            <CDropdownItem href="#">設定檔2</CDropdownItem>
-                        </CDropdownMenu>
-                    </CDropdown>
-                </div>
-            </div>
-        </template> -->
     </div>
 </template>
 
